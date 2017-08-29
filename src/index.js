1e84f23f6b6ef73ead50a4f4e80ac1c82d86f6a7
Tom Cornell <tom.cornell@gmail.com>
2017-08-29 18:17:43 -0400

    Basic game loop
    Added a requestAnimationFrame based game loop. Sprites all
    just rotate their turrets, but it does look like it's supposed to.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,67 @@
 		return document.getElementById(id);
 	}
 
+	// Converts from degrees to radians.
+	Math.radians = function(degrees) {
+	  return degrees * Math.PI / 180;
+	};
+
+	// Converts from radians to degrees.
+	Math.degrees = function(radians) {
+	  return radians * 180 / Math.PI;
+	};
+
+
+	function PlayerAgent(name, startX = 0, startY = 0) {
+		Object.assign(this, {
+			name: name,
+			x: startX,
+			y: startY,
+			dx: 0,
+			dy: 0,
+			aim: Math.random() * 2* Math.PI,	// random initial AIM
+		});
+	}
+
+	PlayerAgent.RADIUS = 15;
+
+	PlayerAgent.prototype.render = function (ctx, index, count) {
+		let color = ((360/count) % 360) * index;
+		// circle
+		ctx.strokeStyle = `hsl(${color}, 50%, 33%)`;
+		ctx.fillStyle = `hsl(${color}, 50%, 67%)`;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, PlayerAgent.RADIUS, 0, 2 * Math.PI);
+
+		ctx.fill();
+		// aim pointer
+		ctx.moveTo(this.x, this.y);
+		let x2 = this.x + PlayerAgent.RADIUS * Math.cos(this.aim);
+		let y2 = this.y + PlayerAgent.RADIUS * Math.sin(this.aim);
+		ctx.lineTo(x2, y2);
+
+		ctx.stroke();		
+	}
+
+	PlayerAgent.prototype.update = function () {
+		this.setAim(this.getAim() + 5);
+	}
+
+	/** 
+	* Return contents of AIM register, converted to degrees.
+	* Returns an integer value.
+	*/
+	PlayerAgent.prototype.getAim = function () {
+		return Math.floor(this.aim * 180 / Math.PI);
+	}
+
+	/**
+	* Set contents of AIM register. Input is in degrees; convert to radians.
+	*/
+	PlayerAgent.prototype.setAim = function (degrees) {
+		this.aim = degrees * Math.PI / 180;
+	}
+
 	/**
 	* Position the agents in botList so that no one else is in their
 	* row or col. That is, initializing AIM to 0, 90, 180, or 270 should
@@ -16,15 +77,18 @@
 	*/
 	function initialPositions(botList) {
 		let n = botList.length;
+		// Make randomized lists of row and column numbers
+		// Robot i will start in the center of row[i], col[i].
 		let rows = Array.from(Array(n).keys());
 		let cols = rows.slice(); // clone array
 		shuffle(rows);
 		shuffle(cols);
-		let cw = Math.floor(ns.arena.width/n),		// column width
-			rh = Math.floor(ns.arena.height/n);		// row height
+		let cw2 = Math.floor(ns.arena.width/(2*n)),		// 1/2 column width
+			rh2 = Math.floor(ns.arena.height/(2*n));	// 1/2 row height
+		// Place bots in the middle of their randomly generated cell
 		botList.map((e,i) => {
-			e.x = (rows[i] * cw) + (cw / 2); 
-			e.y = (cols[i] * rh) + (rh / 2);
+			e.x = (rows[i] * cw2 * 2) + cw2; 
+			e.y = (cols[i] * rh2 * 2) + rh2;
 		});
 	}
 
@@ -54,21 +118,21 @@
 		console.log("Arena:", ns.arena);
 	}
 
+	function addSprites() {
+		/// List of agents we will be running (PCs only, for now)
+		for (var i = 0; i < 6; ++i) {
+			ns.sprites.push(new PlayerAgent(`Agent${i}`));
+		}
+		initialPositions(ns.sprites);
+		console.log("Agents:", ns.sprites);
+	}
+
 	ns.main = () => {
 		console.log("RoboWar starting...");
 		configureArena();
-		/// List of agents we will be running (PCs only, for now)
-		ns.sprites = [
-			{name: "Agent1", x: 0, y: 0},
-			{name: "Agent2", x: 0, y: 0},
-			{name: "Agent3", x: 0, y: 0},
-			{name: "Agent4", x: 0, y: 0},
-			{name: "Agent5", x: 0, y: 0},
-			{name: "Agent6", x: 0, y: 0},
-		];
-		initialPositions(ns.sprites);
-		console.log("Agents:", ns.sprites);
+		addSprites();
 		render();
+		gameLoop();
 	}
 
 	function render() {
@@ -77,22 +141,21 @@
 		let n = ns.sprites.length;
 		for (var i = 0; i < n; ++i) {
 			let s = ns.sprites[i];
-			renderAgent(ctx, s, i, n);
+			s.render(ctx, i, n);
 		} 
 	}
 
-	function renderAgent(ctx, agent, index, count) {
-		let color = ((360/count) % 360) * index;
-		// circle
-		ctx.strokeStyle = `hsl(${color}, 50%, 50%)`;
-		ctx.beginPath();
-		ctx.arc(agent.x, agent.y, 15, 0, 2 * Math.PI);
-		// aim pointer
-		ctx.moveTo(agent.x, agent.y);
-		ctx.lineTo(agent.x + 15, agent.y);
+	function gameLoop() {
+		requestAnimationFrame(gameLoop);
+		update();
+		render();
+	}
 
-		ctx.stroke();
-
+	function update() {
+		let n = ns.sprites.length;
+		for (var i = 0; i < n; ++i) {
+			ns.sprites[i].update();
+		}
 	}
 
 })(window.RoboWar = window.RoboWar || {});
